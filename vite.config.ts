2e821bd7e4a3fb8bce02e15f5fd36a854443400b
Rlesjak <lesjak.robert1@gmail.com>
2023-06-06 21:31:12 +0200

    Konfiguriranje base patha za zapakiranu aplikaciju (electron)

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: path.resolve(__dirname, "./dist/"),
+  base: "./",
   server: { port: 8080 },
   plugins: [vue()],
   resolve: {
