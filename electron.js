c38d639f9c97b9a0f42fdd17a21d907d479e27e6
Rlesjak <lesjak.robert1@gmail.com>
2023-06-06 22:46:44 +0200

    Napravljena ikona i konfigurirano pakiranje elektron aplikacije.

--- electron.js
+++ electron.js
@@ -3,8 +3,9 @@ const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1200,
+    height: 900,
+    autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
--- forge.config.js
+++ forge.config.js
@@ -1,22 +1,60 @@
 module.exports = {
-  packagerConfig: {},
+  packagerConfig: {
+    icon: "./dist/icons/win/icon",
+    ignore: [
+      ".vscode",
+      ".gitignore",
+      ".git",
+      "forge.config.js",
+      "README.md",
+      "src",
+      "mock_server",
+      "node_modules",
+      "public",
+      "^/icon$",
+      "vite.config.ts",
+      "tsconfig.node.json",
+      "tsconfig.json",
+      "tsconfig.app.json",
+      "tailwind.config.js",
+      "postcss.config.js",
+      "env.d.ts"
+    ]
+  },
   rebuildConfig: {},
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        options: {
+          icon: "./dist/icons/win/icon.ico"
+        }
+      },
     },
     {
       name: '@electron-forge/maker-zip',
       platforms: ['darwin'],
+      config: {
+        options: {
+          icon: "./dist/icons/mac/icon.icns"
+        }
+      },
     },
     {
       name: '@electron-forge/maker-deb',
-      config: {},
+      config: {
+        options: {
+          icon: "./dist/icons/png/128x128.png"
+        }
+      },
     },
     {
       name: '@electron-forge/maker-rpm',
-      config: {},
+      config: {
+        options: {
+          icon: "./dist/icons/png/128x128.png"
+        }
+      },
     },
   ],
 };
